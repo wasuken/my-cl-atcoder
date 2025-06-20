fca0ba9ceaac09b6927699dd95283103c07da862
wasuken <wasuken1@gmail.com>
2025-06-20 22:04:31 +0900

    template更新

--- _template.lisp
+++ _template.lisp
@@ -2,6 +2,12 @@
 ;;   (format t "~s~%" (uiop:split-string rl :separator " "))
 ;;   (format t "~a~%" (mapcar #'parse-integer (uiop:split-string rl :separator " ")))
 ;;   )
+;; nを読み取って、n回行を読み取る
+;; (let ((n (parse-integer (read-line))))
+;;   (format t "~a~%" (count-if #'(lambda (x) (string= x "Takahashi"))
+;; 		   (loop for x from 0 to (1- n)
+;; 			 collect (read-line))))
+;;   )
 
 (defun take (n l)
   (if (or (= n 0) (not l))
